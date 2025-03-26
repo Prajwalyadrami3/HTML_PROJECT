1e143ae5a422c57daeecf28a33d1dfa42e023c0b
prajwalkumar <prajwalkumar333t@gmail.com>
2025-03-26 14:37:32 +0530

    added submit button

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function hello(){
 return "hello this is yadrami"
 }
 console.log(hello);
+
+function onSubmit(){
+    console.log("form submitted")
+}
+function onEdit(){
+    alert("file edited success")
+}
